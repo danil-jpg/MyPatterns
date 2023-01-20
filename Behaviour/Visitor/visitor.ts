e33f9a28faa884387a7129fea6cc9bdfa4d4f439
Danil <danil.polovinkin.1994@gmail.com>
2023-01-20 12:16:33 +0300

    added visitor

--- Behaviour/Visitor/visitor.ts
+++ Behaviour/Visitor/visitor.ts
@@ -1,2 +1,55 @@
-// Посетитель — это поведенческий паттерн проектирования,
-//  который позволяет добавлять в программу новые операции, не изменяя классы объектов, над которыми эти операции могут выполняться.
+// Посетитель — это поведенческий паттерн, который позволяет добавить новую операцию для целой иерархии классов,
+//  не изменяя код этих классов.
+
+// Посетитель — это поведенческий паттерн, который позволяет добавить новую операцию для целой иерархии классов, не изменяя код этих классов.
+
+// https://stackoverflow.com/questions/9363582/difference-between-decorator-design-pattern-and-visitor-design-pattern#:~:text=Considered%20Bad%20Design%3F-,Decorator%20works%20on%20an%20object%2C%20Visitor%20works%20on%20composite%20structure,visitor%20is%20Behavioral%20design%20pattern.
+// - различие с декоратором.
+
+abstract class Car {
+  accept(visitor): void {
+    visitor(this);
+  }
+}
+
+class Tesla extends Car {
+  info(): string {
+    return "It is a Tesla car!";
+  }
+}
+
+class Bmw extends Car {
+  info(): string {
+    return "It is a BMW car!";
+  }
+}
+
+class Audi extends Car {
+  info(): string {
+    return "It is an Audi car!";
+  }
+}
+
+function exportInfo(car: Car): void {
+  let res: string = "";
+  if (car instanceof Tesla) {
+    res = `Exported data: ${car.info()}`;
+    console.log(res);
+  }
+  if (car instanceof Bmw) {
+    res = `Exported data: ${car.info()}`;
+    console.log(res);
+  }
+  if (car instanceof Audi) {
+    res = `Exported data: ${car.info()}`;
+    console.log(res);
+  }
+}
+
+const bmw = new Bmw();
+
+const audi = new Audi();
+
+bmw.accept(exportInfo);
+
+// console.log(exportInfo(bmw));
--- Behaviour/Visitor/visitorVanilla.js
+++ Behaviour/Visitor/visitorVanilla.js
@@ -1,44 +1,56 @@
+// Посетитель — это поведенческий паттерн, который позволяет добавить новую операцию для целой иерархии классов,
+//  не изменяя код этих классов.
 
+// Посетитель — это поведенческий паттерн, который позволяет добавить новую операцию для целой иерархии классов, не изменяя код этих классов.
 
-class Visitor {
-  
-	makeOrder(command, order) {
-	  this.command = command;
-	  this.command.writeDownTheOrder(order);
-  
-	  return order;
+// https://stackoverflow.com/questions/9363582/difference-between-decorator-design-pattern-and-visitor-design-pattern#:~:text=Considered%20Bad%20Design%3F-,Decorator%20works%20on%20an%20object%2C%20Visitor%20works%20on%20composite%20structure,visitor%20is%20Behavioral%20design%20pattern.
+// - различие с декоратором.
+
+ class Car {
+	accept(visitor) {
+	  visitor(this);
 	}
   }
   
-
-  
-  class Waiter1  {
-  
-	constructor(receiver) {
-	  this.receiver = receiver;
-	}
-  
-	writeDownTheOrder(order) {
-	  this.receiver.cook(order);
-	}
-  }
-
-  
-  class Kitchener {
-	cook(order){
-	  console.log(`${order} is ready`);
-	}
-  
-	doNothing() {
-	  console.log(`Operation has been stopped`);
+  class Tesla extends Car {
+	info() {
+	  return "It is a Tesla car!";
 	}
   }
   
-  const visitor = new Visitor();
+  class Bmw extends Car {
+	info() {
+	  return "It is a BMW car!";
+	}
+  }
   
-  const kitchener = new Kitchener();
+  class Audi extends Car {
+	info() {
+	  return "It is an Audi car!";
+	}
+  }
   
-  const waiter = new Waiter1(kitchener);
+  function exportInfo(car) {
+	let res = "";
+	if (car instanceof Tesla) {
+	  res = `Exported data: ${car.info()}`;
+	  console.log(res);
+	}
+	if (car instanceof Bmw) {
+	  res = `Exported data: ${car.info()}`;
+	  console.log(res);
+	}
+	if (car instanceof Audi) {
+	  res = `Exported data: ${car.info()}`;
+	  console.log(res);
+	}
+  }
   
-  visitor.makeOrder(waiter, "Some fish chips");
+  const bmw = new Bmw();
+  
+  const audi = new Audi();
+  
+  bmw.accept(exportInfo);
+  
+  // console.log(exportInfo(bmw));
   
